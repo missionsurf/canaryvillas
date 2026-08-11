80fce56729a7229ee273a56646844534973e6ddd
missionsurf <roger@canarypools.com>
2026-08-11 18:34:44 +0100

    fix: use SSL for port 465 SMTP connections

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -4,7 +4,7 @@ import { format } from "date-fns";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: Number(process.env.SMTP_PORT) === 465,
   auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
 });
 
